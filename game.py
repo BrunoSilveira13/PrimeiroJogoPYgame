79dc46056bdad95e65774158072e7461af52f63a
Bruno Silveira <brunosilveira@gmail.com>
2022-06-17 16:30:33 -0300

    movimentação de  objetos

--- game.py
+++ game.py
@@ -7,12 +7,22 @@ pygame.init()
  
 largura = 640
 altura = 480
+x = largura /2
+y = 0
+
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('GAME')
-
+relogio = pygame.time.Clock()
 while True: 
+    relogio.tick(60)
+    tela.fill((0,0,0,))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
+    pygame.draw.rect(tela, (255,0,0), (x,y,50,50))
+    if y >= altura:
+        y = 0
+    y = y + 5
+
     pygame.display.update()
