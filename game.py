dd8622ddf17ca76f782b65398ae43764dd67dfa4
Bruno Silveira <brunosilveira@gmail.com>
2022-06-17 22:51:02 -0300

    adição de sons

--- game.py
+++ game.py
@@ -4,11 +4,17 @@ from pygame.locals import *
 from sys import exit
 from random import randint
 pygame.init()
- 
+
+pygame.mixer.music.set_volume(0.3)
+musica_fundo = pygame.mixer.music.load('sons/musica_de_fundo.mp3') 
+pygame.mixer.music.play(-1)
+
+son_colisao = pygame.mixer.Sound('sons/son_moeda.wav')
+
 largura = 640
 altura = 480
-x = largura /2
-y = altura /2
+x = int(largura /2)
+y = int(altura /2)
 
 x_azul = randint(40,600)
 y_azul = randint(50,430)
@@ -46,6 +52,7 @@ while True:
         x_azul = randint(40,600)
         y_azul = randint(50,430)
         pontos = pontos + 1
+        son_colisao.play()
     tela.blit(texto_formatado, (450,40))
 
     pygame.display.update()
